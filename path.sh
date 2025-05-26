f32435a44014485e239f38769481472e51ca3f66
Autobot37 <andyrights37@gmail.com>
2025-05-26 12:11:53 +0530

    Track only yaml, yml, py, ipynb in specific folders + exp.ipynb, path.sh

---
+++ path.sh
@@ -0,0 +1,5 @@
+ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+#  source SSBware/senv/bin/activate
+#   source senv/bin/activate
+
+MASTER_ADDR=localhost MASTER_PORT=10001 python3 /home/vatsal/NWM/earth-forecasting-transformer/scripts/cuboid_transformer/sevir/train_cuboid_sevir.py --gpus 1 --cfg /home/vatsal/NWM/earth-forecasting-transformer/scripts/cuboid_transformer/sevir/cfg_sevir.yaml --save tmp_sevir3
